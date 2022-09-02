b092d32fae56e5501b40d4759b82675f97e056ad
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-09-02 11:54:01 +0300

    Initial commit

---
+++ Seminar.1/Scrypt.1.py
@@ -0,0 +1,10 @@
+day = int(input('Введите номер дня: '))
+print(day)
+while day <= 0 or day > 7:
+    print('Введено неверное значение')
+    day = int(input('Введите номер дня: '))
+else:
+    if day >=1 or day <= 5:
+        print ('Нет')
+    else:
+        print ('Да')
