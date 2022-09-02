71454e1f72e4dec855f67ea1de54588f4aeb7b80
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-09-02 13:16:20 +0300

    Задача 3 решена

---
+++ Seminar.1/Scrypt.3.py
@@ -0,0 +1,11 @@
+dot = [int(input('Введите X:')), int(input('Введите Y:'))]
+if(dot[0] == 0 and dot[1] == 0):
+    print(f'dot({dot[0]},{dot[1]}) - недопустимое значение')
+elif(dot[0] >= 0 and dot[1] >= 0):
+    print(f'dot({dot[0]},{dot[1]}) - 1')
+elif(dot[0] <= 0 and dot[1] >= 0):
+    print(f'dot({dot[0]},{dot[1]}) - 2')
+elif(dot[0] <= 0 and dot[1] <= 0):
+    print(f'dot({dot[0]},{dot[1]}) - 3')
+else:
+    print(f'dot({dot[0]},{dot[1]}) - 4')
