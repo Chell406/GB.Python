32be3883ea53f48d9e08e68fe01fcffff3ef7d4e
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-09-02 13:20:20 +0300

    Заменены '-' на '->' в выводе

--- Seminar.1/Scrypt.3.py
+++ Seminar.1/Scrypt.3.py
@@ -2,10 +2,10 @@ dot = [int(input('Введите X:')), int(input('Введите Y:'))]
 if(dot[0] == 0 and dot[1] == 0):
     print(f'dot({dot[0]},{dot[1]}) - недопустимое значение')
 elif(dot[0] >= 0 and dot[1] >= 0):
-    print(f'dot({dot[0]},{dot[1]}) - 1')
+    print(f'dot({dot[0]},{dot[1]}) -> 1')
 elif(dot[0] <= 0 and dot[1] >= 0):
-    print(f'dot({dot[0]},{dot[1]}) - 2')
+    print(f'dot({dot[0]},{dot[1]}) -> 2')
 elif(dot[0] <= 0 and dot[1] <= 0):
-    print(f'dot({dot[0]},{dot[1]}) - 3')
+    print(f'dot({dot[0]},{dot[1]}) -> 3')
 else:
-    print(f'dot({dot[0]},{dot[1]}) - 4')
+    print(f'dot({dot[0]},{dot[1]}) -> 4')
