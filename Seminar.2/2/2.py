0ff068a32ff33bf77c8e178f804b28a8ba237ac0
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-09-11 16:55:59 +0300

    Заменена работа с файлами на конструкцию 'with open() as ...'

--- Seminar.2/2/2.py
+++ Seminar.2/2/2.py
@@ -1,11 +1,13 @@
-inputFile = open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2\INPUT.txt','r')
-N = inputFile.readline()
-inputFile.close()
+with open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2\INPUT.txt', 'r') as inputFile:
+    N = inputFile.readline()
+    inputFile.close()
+
 print(f'N = {N}')
 summary = 0
 for i in range(1, int(N) + 1):
     summary += i
 print(f'Summary = {summary}')
-outputFile = open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2\OUTPUT.txt','w')
-outputFile.write(str(summary))
-outputFile.close()
+
+with open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2\OUTPUT.txt', 'w') as outputFile:
+    outputFile.write(str(summary))
+    outputFile.close()
