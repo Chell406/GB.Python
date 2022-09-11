54e949bacbff25b9ce1325b33b7e6eb856988f15
Maksim Olehnovich <maksm.olehnovich@tut.by>
2022-09-11 15:17:17 +0300

    Задача 2 решена

---
+++ Seminar.2/2.py
@@ -0,0 +1,11 @@
+inputFile = open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2.INPUT.txt','r')
+N = inputFile.readline()
+inputFile.close()
+print(f'N = {N}')
+summary = 0
+for i in range(1, int(N) + 1):
+    summary += i
+print(f'Summary = {summary}')
+outputFile = open('C:\D\Скачки\GeekBrains\GB.PY\Seminar.2\\2.OUTPUT.txt','w')
+outputFile.write(str(summary))
+outputFile.close()
